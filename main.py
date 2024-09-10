026c39b504a5b03dffff1f104a71c51b24e18e32
0xb0c4n <womercode@gmail.com>
2024-09-10 09:16:27 +0200

    ajout

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import PIL.Image
 import math
 
-def get_bin(src="3-james_bond.png"):
+def get_bin_img(src="3-james_bond.png"):
     """Prend en compte le nom du fichier image source (str)
     Renvoie le tableau binaire de toutes les couleurs"""
     img = PIL.Image.open(src)
     width, height = img.size
     array=[]
-    for x in range(width+1):
+    for y in range(height):
         rank = []
-        for y in range(height+1):
+        for x in range(width):
             r,v,b = img.getpixel((x,y))
-            rank.append()
+            color = [bin(r)[2:],bin(v)[2:],bin(b)[2:]]
+            rank.append(color)
+        array.append(rank)
+    return array
+
+def get_bin_msg(src="3-message.txt"):
+    with open(src) as f:
+        print(f.read())
+
+get_bin_msg()
