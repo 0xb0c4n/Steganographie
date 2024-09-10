dc08ff193901804abb2cc8979f25bd031d645a94
0xb0c4n <womercode@gmail.com>
2024-09-10 10:02:32 +0200

    Début de la fonction d'insertion

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def get_bin_img(src="3-james_bond.png"):
     img = PIL.Image.open(src) 
     width, height = img.size
     array = []
-    for y in range(height):
-        rank = []
-        for x in range(width):
+    for x in range(width):
+        column = []
+        for y in range(height):
             r,v,b = img.getpixel((x,y))
             color = [bin(r)[2:],bin(v)[2:],bin(b)[2:]]
-            rank.append(color)
-        array.append(rank)
+            column.append(color)
+        array.append(column)
     return array
 
 def get_bin_msg(src="3-message.txt"):
@@ -22,14 +22,22 @@ def get_bin_msg(src="3-message.txt"):
         msg = f.read()
         for letter in msg:
             array.append(bin(ord(letter))[2:])
-    return array
+    return array, len(msg)
 
 def add_zeros(array):
     for i in range(len(array)):
         if(len(array[i]) < 8):
             for j in range(8-len(array[i])):
                 array[i] = "0" + array[i]
-    print(array)
+    return array
 
-array = get_bin_msg()
-add_zeros(array)
+def insertion(length_msg, img_array, array):
+    #Ajout des informations de codage
+    for i in range(5):
+        for j in range(len(img_array[0][i])):
+            length_in_bin = bin(length_msg)[2:]
+ 
+img_array = get_bin_img()
+array, length_msg = get_bin_msg()
+array = add_zeros(array)
+insertion(length_msg, img_array, array)
