7d04312b8d926e0a82c2a8d4ab16b852e23ca92e
0q3i <nicolas521janini@gmail.com>
2024-09-10 09:45:18 +0200

    la fonction binaire est fini et ajout du deput de la fonction decyptage

--- decytage.py
+++ decytage.py
@@ -1,21 +1,31 @@
 import PIL.Image as PIL 
 
-def binaire(image):
-    """Renvois un str avec le message cacher dans l'image"""
+
+def binaire(image='3-james_bond.png'):
+    """Renvois une list avec 
+    la position du pixel et la couleur du pixel en binaire"""
     img = PIL.open(image)
     h , l = img.size
     h_compte , l_compte = 0 , 0
     lst_bin = []
-    while h_compte <= h and l_compte <= l:
+    while h_compte < h and l_compte < l:
         r, v, b = img.getpixel((h_compte,l_compte))
         bin_colR = bin(r)[2:]
         bin_colV = bin(v)[2:]
         bin_colB = bin(b)[2:]
-        dic_binCol = {'pixel : ': (h_compte, l_compte),' couleur : ': (r, v, b), 'bin : ': [bin_colR, bin_colV, bin_colB] }
+        dic_binCol = {'pixel : ': (h_compte, l_compte),'bin : ': [bin_colR, bin_colV, bin_colB] }
         lst_bin.append(dic_binCol)
         h_compte +=1
         l_compte +=1
     return lst_bin
 
-
+def decyptage(lst_bin='binaire', image = '3-james_bond'):
+    """Renvois un str avec le mssageinscris dans l'image"""
+    
+    h_compte = 0
+    l_compte = 0
+    """
+        h_compte +=1
+        l_compte +=1
+"""
 print(binaire('3-james_bond.png'))
