835ce2df6af15e351d68ddc75f7df026e9978b2d
0xb0c4n <womercode@gmail.com>
2024-09-10 08:48:46 +0200

    Boucle pour avoir tout les pixels

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from PIL.Image import *
+import PIL.Image
 import math
+
+def get_bin(src="3-james_bond.png"):
+    """Prend en compte le nom du fichier image source (str)
+    Renvoie le tableau binaire de toutes les couleurs"""
+    img = PIL.Image.open(src)
+    width, height = img.size
+    for x in range(width+1):
+        for y in range(height+1):
+            r,v,b = img.getpixel((x,y))
