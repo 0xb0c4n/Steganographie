a081a414c119e87e51d1f215e1e1e18a6d3d00e4
0xb0c4n <womercode@gmail.com>
2024-09-12 11:46:06 +0200

    Insertion

--- main.py
+++ main.py
@@ -24,20 +24,30 @@ def get_bin_msg(src="3-message.txt"):
             array.append(bin(ord(letter))[2:])
     return array, len(msg)
 
-def add_zeros(array):
-    for i in range(len(array)):
-        if(len(array[i]) < 8):
-            for j in range(8-len(array[i])):
-                array[i] = "0" + array[i]
+def add_zeros(array, nb):
+    if(type(array) == list):
+        for i in range(len(array)):
+            if(len(array[i]) < nb):
+                for j in range(nb-len(array[i])):
+                    array[i] = "0" + array[i]
+    else:
+        for i in range(len(array)):
+            if(len(array) < nb):
+                array = "0" + array
+
     return array
 
 def insertion(length_msg, img_array, array):
     #Ajout des informations de codage
+    index = 0
     for i in range(5):
         for j in range(len(img_array[0][i])):
             length_in_bin = bin(length_msg)[2:]
- 
+            length_in_bin = add_zeros(length_in_bin, 15)
+            print(length_in_bin)
+            print(img_array[0][i][j][:-1] + length_in_bin[j])
+
 img_array = get_bin_img()
 array, length_msg = get_bin_msg()
-array = add_zeros(array)
+array = add_zeros(array, 8)
 insertion(length_msg, img_array, array)
